abe8268aaac2d4e6bf3bbe07ea41e8bb5b3d686e
pannawit2541 <35355034+pannawit2541@users.noreply.github.com>
2020-08-21 13:29:59 +0700

    fix bug

--- Assignment 1/_backup/Cross_Prediction.py
+++ Assignment 1/_backup/Cross_Prediction.py
@@ -52,7 +52,7 @@ class NeuralNetwork(object):
             v = np.dot(activations, w)
             # calculate the activations
             b = self.bias[i]
-            activations = self.sigmoid(v+b)
+            activations = self.sigmoid(v)
             self.activations[i+1] = activations
         return activations
 
@@ -96,12 +96,15 @@ class NeuralNetwork(object):
                 error = target - output
                 print(output, " - ", target)
                 if i > 0 :
+                    self.backPropagate(error)
                     self.derivatives_old = copy.deepcopy(self.derivatives)
-                self.backPropagate(error)
+                else:
+                    self.derivatives_old = copy.deepcopy(self.derivatives)
+                    self.backPropagate(error)
+                    
                 # now perform gradient descent on the derivatives
                 # (this will update the weights
-                if i == 0:
-                    self.derivatives_old = copy.deepcopy(self.derivatives)
+                    
                 self.gradient_descent(learning_rate,momentumRate)
 
                 # keep track of the MSE for reporting later
@@ -109,6 +112,7 @@ class NeuralNetwork(object):
 
             # Epoch complete, report the training error
             print("Error: {} at epoch {}".format(round(sum_errors / len(X) , 5), i+1))
+        self.sum_all_err = sum_errors / len(X)
         print("Training complete! : ",sum_errors/len(X))
         print("=====")
 
@@ -121,8 +125,6 @@ class NeuralNetwork(object):
             derivatives_old  = self.derivatives_old[i]
             delta = (derivatives * learningRate) + ((derivatives-derivatives_old)*momentumRate)
             weights += delta
-            delta = np.dot(delta.T,np.ones(delta.T.shape[1]))
-            bias += delta
 
     def _mse(self, target, output):
         return np.average((target - output) ** 2)
@@ -202,6 +204,18 @@ def cross_validations_split(dataset,output_dataset,folds):
 
 #X, Y, inputSizeX, outputSizeY = Preprocessing()
 A, B, inputSizeA, outputSizeB = Preprocessing_Cross()
+class_0 = 0
+class_1 = 0
+
+for i in range(B.shape[0]):
+    print( B[i][0])
+    if B[i][0] == 1:
+        class_0 +=1
+    else:
+        class_1 +=1
+
+print(class_0)
+print(class_1)
 #max,min = Y.max(),Y.min()
 #y = convert_output(max,min,Y)
 #x = convert_input(X)
@@ -213,14 +227,14 @@ print(" -- Hidden layer have 3 layers and 4,2,2 nodes respectively -- ")
 
 
 
-hiddenSizeStr = '3'
+hiddenSizeStr = '6'
 
 hiddenSize = hiddenSizeStr.split("-")
 hiddenSize = list(map(int, hiddenSize))
 #index = cross_validations_split(x,y,10)
-index = cross_validations_split(A,B,10)
+index_cross = cross_validations_split(A,B,10)
 #NN = NeuralNetwork(hiddenSize, inputSizeX, outputSizeY)
-NN = NeuralNetwork(hiddenSize, inputSizeA, outputSizeB)
+NN_cross = NeuralNetwork(hiddenSize, inputSizeA, outputSizeB)
 
 """
 for a,b in index:
@@ -230,9 +244,50 @@ for a,b in index:
     print(np.sum(NN._mse(NN.feedForward(x[a:b,:]),y[a:b,:]),axis=0)) 
  
 """
-for a,b in index:
+'''
+sum_avg_train = 0
+sum_avg_predict = 0
+for a,b in index_cross:
     inTest = np.concatenate((A[:a],A[b+1:]))
-    outTest = np.concatenate((A[:a],B[b+1:]))
-    NN.train(inTest, outTest, 1000, 0.8,0.2)
-    print(np.sum(NN._mse(NN.feedForward(A[a:b+1,:]),B[a:b+1,:]),axis=0)) 
+    outTest = np.concatenate((B[:a],B[b+1:]))
+    NN_cross.train(inTest, outTest, 1000 , 0.8  ,0.2)
+    sum_avg_train += NN_cross.sum_all_err
+    sum_avg_predict += np.sum(NN_cross._mse(NN_cross.feedForward(A[a:b,:]),B[a:b,:]),axis=0)
 
+
+
+print(sum_avg_train/10)
+print(sum_avg_predict/10)
+predict = NN_cross.feedForward(A)
+label_data = []
+label_predict = []
+for i in range(A.shape[0]):
+    if predict[i][0] > predict[i][1]:
+        label_predict.append(0)
+    elif predict[i][0] <= predict[i][1]:
+        label_predict.append(1)
+    if B[i][0] > B[i][1]:
+        label_data.append(0)
+    elif B[i][0] <= B[i][1]:
+        label_data.append(1)
+
+a1 = 0
+a2 = 0
+b1 = 0
+b2 = 0
+for i in range(len(label_data)):
+    if label_data[i] == 0:
+        if label_predict[i] != label_data[i]:
+            a2 += 1
+        else:
+            a1 += 1
+    else:
+        if label_predict[i] != label_data[i]:
+            b1 += 1
+        else:
+            b2 += 1
+
+print("== 0  ==== 1")
+print("0 = ",a1," == ",a2)
+print("1 = ",b1," == ",b2)
+'''
